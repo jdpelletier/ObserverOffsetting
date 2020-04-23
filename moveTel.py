1a0e1b0c1d97be393e525364c11e72d835341316
jdpelletier <john.pelletier@ymail.com>
2020-04-23 10:49:40 -1000

    outlining plan for moveTel

--- moveTel.py
+++ moveTel.py
@@ -1,3 +1,20 @@
 from OffsetLib import *
 
-print("loaded ok")
+##print options
+
+##get response
+
+##get arguments, based on response
+
+##execute funtion from dictionary
+
+##continue while loop until exiting
+
+def moveFrame(i):
+    switcher={
+        1:azel.azel,
+        2:en.en,
+        3:gxy.gxy,
+    return switcher.get(i,"Invalid choice")
+
+moveframe(i)(arguments)
