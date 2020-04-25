c2d9c6bcaccffad156691804d5ce8635300c4b69
jdpelletier <john.pelletier@ymail.com>
2020-04-24 15:54:26 -1000

    fixing lots of issues, function selecting is all working

--- moveTel.py
+++ moveTel.py
@@ -1,8 +1,21 @@
 import sys
+import os
+import platform
 
 import ktl
 
-from OffsetLib import *
+from OffsetLib import azel
+from OffsetLib import en
+from OffsetLib import gxy
+from OffsetLib import mxy
+from OffsetLib import mark
+from OffsetLib import gomark
+from OffsetLib import markbase
+
+if platform.system() == "Windows":
+    os.system('cls')
+else:
+    os.system('clear')
 
 
 ##cache instrument
@@ -34,7 +47,7 @@ def getGscale(instrument):
 
 ##Function to print instructions and get command from user.
 def getCommand():
-    response = input(
+    print(
         '''
         Choose a command from the list below:
         1: Move in the AZ-EL direction
@@ -46,20 +59,22 @@ def getCommand():
         7: Mark current position
         8: Go to previously marked position
         9: Mark base
-        10: Quit (crtl-c) \n >
+        10: Quit (crtl-c) \n
         '''
         )
-    return input
+    response = input('>>> ')
+    return int(response)
 ##
 
 
 ##get arguments, based on response
 def executeCommand(command):
-    if command in range(1:4):
+    if command in range(1, 5):
         return getXY(command)
-    elif command in range(5:6):
+    elif command in range(5, 7):
         return getSpecMove(command)
-    elif command in range(7-10):
+    elif command in range(7 ,11):
+        command = command - 2
         moveFrame(command)()
         return True
     else:
@@ -69,27 +84,28 @@ def executeCommand(command):
 
 ##If the command only needs x and y coords, run this function.
 def getXY(command):
-    abs = False
     coordString = input('Enter offset numbers as x y (no comma): ')
     coords = coordString.split()
     while len(coords) != 2 or ',' in coordString:
-        startString = input('Error: Enter offset numbers as x y (no comma): ')
+        coordString = input('Error: Enter offset numbers as x y (no comma): ')
         coords = coordString.split()
-    x = start[0]
-    y = start[1]
+    x = int(coords[0])
+    y = int(coords[1])
     if command == 3:
         argTuple = (False, x, y)
     elif command == 4:
-        absString = input(
+        abs = False
+        print(
             '''
             Move relative to:
             1: Current position
             2: Base
             '''
             )
-        while absString != '1' or '2'
+        absString = int(input('>>> '))
+        while absString not in [1, 2]:
             absString = input("Error: Enter 1 (current) or 2 (base): ")
-        if absString = '2':
+        if absString == 2:
             abs = True
         argTuple = (False, abs, x, y)
     else:
@@ -115,26 +131,29 @@ def getSpecMove(command):
     while len(end) != 2 or ',' in endString:
         endString = input('Error: Enter input as x y (no comma): ')
         end = endString.split()
-    moveString = input(
+    print(
         '''
         Do you want to:
         1: Send moves
         2: Check offset
         '''
         )
-    while moveString != '1' or '2'
+    moveString = int(input('>>> '))
+    while moveString not in [1, 2]:
         moveString = input("Error: Enter 1 (send move) or 2 (no move): ")
-    if moveString == '2':
+    if moveString == 2:
         nomove = True
     if command == 5:
         gscale = getGscale()
-        dx = gscale * (start[0]-end[0])
-        dy = gscale * (end[1]-start[1])
+        dx = gscale * (int(start[0])-int(end[0]))
+        dy = gscale * (int(end[1])-int(start[1]))
+        command = 3
         argTuple = (nomove, dx, dy)
     else:
         pscale = getPscale()
-        dx = pscale * (start[0]-end[0])
-        dy = pscale * (end[1]-start[1])
+        dx = pscale * (int(start[0])-int(end[0])
+        dy = pscale * (int(end[1])-int(start[1]))
+        command = 4
         argTuple = (nomove, False, dx, dy)
     moveFrame(command)(*argTuple)
     return True
@@ -142,7 +161,7 @@ def getSpecMove(command):
 
 ##Quitting program
 def endprogram():
-    print('Ending Telescope Controller...')
+    print('\nEnding Telescope Controller...\n')
     sys.exit()
 ##
 
@@ -155,7 +174,7 @@ def moveFrame(i):
         4:mxy.mxy,
         5:mark.mark,
         6:gomark.gomark,
-        7:markbase.markbase
+        7:markbase.markbase,
         8:endprogram
         }
     return switcher.get(i,"Invalid choice")
@@ -177,4 +196,4 @@ if __name__=='__main__':
         while True:
             main()
     except KeyboardInterrupt:
-            endprogram
+            endprogram()
